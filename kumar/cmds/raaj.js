7262c5445101b287112419c2cd54bbc17850a4f2
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2025-01-15 18:32:54 +0200

    Update raaj.js

--- kumar/cmds/raaj.js
+++ kumar/cmds/raaj.js
@@ -4,7 +4,7 @@ const path = require('path');
 const logger = require('../../includes/logger');
 
 module.exports.config = {
-    name: "raaj",
+    name: "rk",
     admin: false,
     description: "Chat with Raaj Ai",
     credits: "Z3R0SEC"
@@ -15,7 +15,7 @@ module.exports.run = async ({ api, event, args }) => {
     const body = args.join(" ");
 
     if (!body) {
-        return api.sendMessage("Yes?", event.threadID);
+        return api.sendMessage("Yes ?", event.threadID);
     }
 
     try {
@@ -33,69 +33,49 @@ module.exports.run = async ({ api, event, args }) => {
         if (data.reply) {
             api.sendMessage(data.reply, event.threadID);
         }
-
-        // Function to handle file downloading
-        const downloadFile = async (url, filePath) => {
-            const response = await axios({
-                url,
-                method: 'GET',
-                responseType: 'stream'
-            });
-            const writer = fs.createWriteStream(filePath);
-            response.data.pipe(writer);
-
-            return new Promise((resolve, reject) => {
-                writer.on('finish', resolve);
-                writer.on('error', reject);
-            });
-        };
-
-        // If there's an image to download and send
         if (data.image) {
             const imagePath = path.join(__dirname, 'cache/kuma.jpg');
+            const writer = fs.createWriteStream(imagePath);
+            const imageResponse = await axios.get(data.image, { responseType: 'stream' });
 
-            try {
-                // Download the image to the cache folder
-                await downloadFile(data.image, imagePath);
-                logger('Image downloaded successfully', 'info');
-
-                // Send the image and then delete it
+            imageResponse.data.pipe(writer);
+            writer.on('finish', () => {
                 api.sendMessage({
                     body: "",
                     attachment: fs.createReadStream(imagePath)
                 }, event.threadID, () => {
                     fs.unlinkSync(imagePath);
-                    logger('Image deleted after sending', 'info');
                 });
+            });
 
-            } catch (err) {
-                logger(`Error downloading or sending image: ${err.message}`, 'error');
+            writer.on('error', (err) => {
+                logger(`Error downloading image: ${err.message}`, 'error');
                 api.sendMessage("Error sending requested photo", event.threadID);
-            }
+            });
         }
 
-        // If there's a song to download and send
         if (data.song) {
             const songPath = path.join(__dirname, 'cache/song.mp3');
+            const songResponse = await axios.get(data.song, { responseType: 'stream' });
 
-            try {
-                // Download the song to the cache folder
-                await downloadFile(data.song, songPath);
-                logger('Song downloaded successfully', 'info');
+            const songWriter = fs.createWriteStream(songPath);
+            songResponse.data.pipe(songWriter);
 
-                // Send the song and then delete it
+            songWriter.on('finish', () => {
+                logger("Done Downloading The Song!","success");
+                const msg = "Here is your song dawg!";
                 api.sendMessage({
-                    body: "Here is your song!",
+                    body: msg,
                     attachment: fs.createReadStream(songPath)
                 }, event.threadID, () => {
                     fs.unlinkSync(songPath);
-                    logger('Song deleted after sending', 'info');
                 });
+            });
 
-            } catch (err) {
-                logger(`Error downloading or sending song: ${err.message}`, 'error');
-                api.sendMessage("Error sending the song", event.threadID);
-            }
+            songWriter.on('error', (err) => {
+                logger(`Error downloading song: ${err.message}`, 'error');
+                api.sendMessage("Error downloading the song.", event.threadID);
+            });
         }
 
     } catch (error) {
