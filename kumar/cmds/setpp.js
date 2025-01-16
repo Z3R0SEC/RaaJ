e561b06669d3a099b316674115d5f7475df2dfa8
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2025-01-16 08:08:48 +0200

    Create setpp.js

---
+++ kumar/cmds/setpp.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const axios = require("axios");
+
+module.exports.config = {
+    name: "setpp",
+    admin: false,
+    description: "Sets the bot's profile picture",
+    credits: "Z3R0SEC"
+};
+
+module.exports.run = async ({ api, event }) => {
+    // Check if a message was replied to and if it contains attachments
+    if (event.messageReply && event.messageReply.attachments.length > 0) {
+        const attachment = event.messageReply.attachments[0];
+        
+        // Make sure the attachment is an image
+        if (attachment.type === "photo") {
+            const imageUrl = attachment.url;
+            
+            try {
+                // Download the image
+                const response = await axios({
+                    url: imageUrl,
+                    responseType: "stream"
+                });
+                
+                const filePath = __dirname + "/temp_profile_pic.jpg";
+                
+                // Save the image to a local file
+                const writer = fs.createWriteStream(filePath);
+                response.data.pipe(writer);
+
+                writer.on("finish", () => {
+                    // Once the image is downloaded, set it as the profile picture
+                    api.changeProfilePicture(fs.createReadStream(filePath), (err) => {
+                        if (err) {
+                            api.sendMessage("Failed to change profile picture.", event.threadID);
+                        } else {
+                            api.sendMessage("Profile picture updated successfully!", event.threadID);
+                        }
+
+                        // Delete the local file after use
+                        fs.unlinkSync(filePath);
+                    });
+                });
+
+                writer.on("error", () => {
+                    api.sendMessage("Error saving the image file.", event.threadID);
+                });
+
+            } catch (error) {
+                api.sendMessage("Failed to download the image.", event.threadID);
+            }
+
+        } else {
+            api.sendMessage("Please reply to a photo to set as the profile picture.", event.threadID);
+        }
+
+    } else {
+        api.sendMessage("You need to reply to a message with an image to set it as the profile picture.", event.threadID);
+    }
+};
