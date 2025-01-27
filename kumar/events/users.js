7e1c7b7bec84f1e3e3bd65d8cda39e35c8f0859c
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2025-01-27 16:54:56 +0200

    Update users.js

--- kumar/events/users.js
+++ kumar/events/users.js
@@ -1,3 +1,4 @@
+const { readConfig, writeConfig } = require('../../includes/handleConfig');
 const logger = require('../../includes/logger');
 const axios = require("axios");
 const fs = require('fs');
@@ -11,15 +12,25 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event }) => {
     try {
+        const config = readConfig();
         const botID = await api.getCurrentUserID();
+        const threadID = event.threadID;
+
+        // Add group ID to config if the bot is added to a new group
+        if (!config.groupIds.includes(threadID)) {
+            config.groupIds.push(threadID);
+            writeConfig(config);
+            logger(`Added new group ID: ${threadID} to config`, "info");
+        }
+
         const userID = event.logMessageData.addedParticipants[0].userFbId;
         const userName = event.logMessageData.addedParticipants[0].firstName || 'New Member';
         const members = event.participantIDs;
         const authinfo = await api.getUserInfo(event.author);
         const author = authinfo[event.author].name;
+
         let gif = "https://i.pinimg.com/originals/65/eb/8a/65eb8a6b4a8966bafd26d1dee21ed97f.gif";
         let newGif = `https://kaiz-apis.gleeze.com/api/welcomeV2?nickname=${userName}&secondText=You're%20user%20${members.length}&avatar=https://graph.facebook.com/${userID}/picture?access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
-
 
         const img = path.join(__dirname, 'image.jpg');
         const writer = fs.createWriteStream(img);
